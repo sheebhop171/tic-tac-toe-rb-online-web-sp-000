301a28dd3348334ee15ad8f77e7d476e6555ca3a
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-11 17:51:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,11 @@ def move(board, position, token)
   index = position-1
   board[index] = token 
 end
+
+def position_taken?(board,index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else board[index] != " "
+    false
+  end
+end
